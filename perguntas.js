135738201a8f9a547e31131f34ff5d22932d2b9a
dacutsbyh <degrossolidavi@gmail.com>
2024-11-21 13:38:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é a maior cobra do Brasil?',
+    'A maior cobra do Brail é a sucuri'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é o maior peixe do Brasil',
+    'pirarucu e piraiba'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é o peixe masi perigoso do brasil',
+    'paixe-escorpião'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é a cobra mais venenosa do Brasil',
+    'sururucu'
+)
